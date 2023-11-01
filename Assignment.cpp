c3437b1ad58105e65fe83bcfb00de74f3e10305c
HoorAhmed1 <hourahmed11@gmail.com>
2023-11-01 16:26:07 +0200

    Average function

--- Assignment.cpp
+++ Assignment.cpp
@@ -9,8 +9,14 @@ int getSum(std::vector<int> elements){
     return sum;
 }
 
+int getAverage(std::vector<int> elements){
+    int sum = getSum(elements);
+    return sum / elements.size();
+}
+
 int main(){
     std::vector<int> elements = {5,2,3,4,1,6,8,7,8,5,1};
     std::cout << getSum(elements) << std::endl;
+    std::cout << getAverage(elements) << std::endl;
     return 0;
 }
