6cf27f0df8a304ed8d67f50d434cbb48a5be2f8e
HoorAhmed1 <hourahmed11@gmail.com>
2023-11-01 16:31:48 +0200

    Min function

--- Assignment.cpp
+++ Assignment.cpp
@@ -8,9 +8,18 @@ int getSum(std::vector<int> elements){
     }
     return sum;
 }
-
+int getMin(std::vector<int> elements){
+    int min = elements[0];
+    for (int i = 0; i < elements.size(); i++){
+        if (elements[i] < min){
+            min = elements[i];
+        }
+    }
+    return min;
+}
 int main(){
     std::vector<int> elements = {5,2,3,4,1,6,8,7,8,5,1};
     std::cout << getSum(elements) << std::endl;
+    std::cout << getMin(elements) << std::endl;
     return 0;
 }
